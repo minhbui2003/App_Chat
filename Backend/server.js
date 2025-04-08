22e9e2b4f1b8db6c7d52dd2633074ad389c574e2
Bui Tat Nhut Minh <minhbui88888888@gmail.com>
2025-04-08 10:12:01 +0700

    Add: server.js

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+connectDB();
+
+server.listen(3000, () => console.log('Server running on port 3000'));
